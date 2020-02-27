97fd3906e69fc932d660968be8e2efa2e4091473
Sam Harwell <Sam.Harwell@microsoft.com>
2020-02-27 08:50:02 -0800

    Update license headers in generated files

--- src/Servers/Kestrel/tools/CodeGenerator/HttpUtilities/HttpUtilities.cs
+++ src/Servers/Kestrel/tools/CodeGenerator/HttpUtilities/HttpUtilities.cs
@@ -60,8 +60,9 @@ namespace CodeGenerator.HttpUtilities
             int knownMethodsArrayLength = (int)(Math.Pow(2, maskLength) + 1);
             int methodNamesArrayLength = httpMethods.Length;
 
-            return string.Format(@"// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+            return string.Format(@"// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 using System;
 using System.Runtime.CompilerServices;
